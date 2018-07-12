44d440519c6ee8d97f8153d362280707f42f08ca
kanishkabhat7 <kanishkabhat7@gmail.com>
2018-07-12 23:31:43 +0530

    hello

--- php/database/connect.php
+++ php/database/connect.php
@@ -4,7 +4,7 @@ error_reporting(0);
 $user = 'root';
 $pass = 'transport';
 $url = 'localhost';
-$db_name = 'bus';
+$db_name = '';
 
 // Create connection
 $conn = mysqli_connect($url, $user, $pass, $db_name);
